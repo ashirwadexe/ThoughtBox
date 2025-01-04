9b8efb65fe0fbedef1608550b467445c3035872d
ashirwadexe <ashirwadbappy@gmail.com>
2025-01-04 22:08:44 +0530

    initialised the frontend of ThoughtBox App

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-red-500">Ashirwad</h1>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
